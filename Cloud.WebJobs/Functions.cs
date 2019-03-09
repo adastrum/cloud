db9ff82a5bab083988187e56e967e4180cf01dd0
Dmitry Trofimov <trofimovde@outlook.com>
2019-03-09 20:19:00 +0300

    Add web job with command queue trigger

---
+++ Cloud.WebJobs/Functions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace Cloud.WebJobs
+{
+    public class Functions
+    {
+        public static void ProcessQueueMessage([QueueTrigger("commandqueue")] string message, ILogger logger)
+        {
+            logger.LogInformation(message);
+        }
+    }
+}
---
+++ Cloud.WebJobs/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Cloud.WebJobs
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            var builder = new HostBuilder();
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+                b.AddAzureStorage();
+            });
+            builder.ConfigureLogging((context, b) =>
+            {
+                b.AddConsole();
+            });
+            var host = builder.Build();
+            using (host)
+            {
+                host.Run();
+            }
+        }
+    }
+}
