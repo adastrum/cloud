7987dc9c549aa2887117df3a2bdcd7e972d95bcc
Dmitry Trofimov <trofimovde@outlook.com>
2019-03-24 19:36:58 +0300

    Use queue output in create order function instead of queue client

--- Cloud.Functions/OnCreateOrderHttpFunction.cs
+++ Cloud.Functions/OnCreateOrderHttpFunction.cs
@@ -3,10 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Queue;
 using Newtonsoft.Json;
 using System.Threading.Tasks;
 
@@ -18,36 +15,17 @@ namespace Cloud.Functions
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = "orders")] HttpRequest req,
             ILogger log,
-            ExecutionContext context)
+            [Queue("commandqueue")]IAsyncCollector<CreateOrderCommand> outputQueue)
         {
             var body = await req.ReadAsStringAsync();
 
             var model = JsonConvert.DeserializeObject<CreateOrder>(body);
 
+            //todo: validation?
+
             var command = new CreateOrderCommand(model.Description, model.Amount.Value);
 
-            var message = JsonConvert.SerializeObject(command,
-                new JsonSerializerSettings
-                {
-                    TypeNameHandling = TypeNameHandling.All
-                });
-
-            var config = new ConfigurationBuilder()
-                .SetBasePath(context.FunctionAppDirectory)
-                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
-                .AddEnvironmentVariables()
-                .Build();
-
-            var cloudStorageAccountConnectionString = config["CloudStorageAccountConnectionString"];
-            var storageAccount = CloudStorageAccount.Parse(cloudStorageAccountConnectionString);
-            var cloudQueueClient = storageAccount.CreateCloudQueueClient();
-            var queue = cloudQueueClient.GetQueueReference("commandqueue");
-
-            await queue.CreateIfNotExistsAsync();
-
-            var cloudQueueMessage = new CloudQueueMessage(message);
-
-            await queue.AddMessageAsync(cloudQueueMessage);
+            await outputQueue.AddAsync(command);
 
             return new OkResult();
         }
